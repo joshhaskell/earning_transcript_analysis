7073f9da29d7425e1e186528ec8929d1d4b74f43
Josh Haskell <jbradhaskell@gmail.com>
2024-01-09 12:05:34 -0500

    Completes prices eda
    - Updates utils.py file with new functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 import string
 import nltk
 from nltk.corpus import stopwords
@@ -56,3 +57,35 @@ def count_entity_types(entities):
     """Extracts and Counts occurrences of different types of named entities in a list of tupples of word and entity pairings."""
     entity_types = [ent[1] for ent in entities]
     return pd.Series(entity_types).value_counts()
+
+# functions for looking at volatility around the earnings call periods
+def calculate_volatility(data, window=5):
+    """Calculate the rolling standard deviation of a given Series."""
+    return data.rolling(window=window).std()
+
+def prepare_volatility_data(df, ticker, call_dates):
+    """Prepare the volatility data for a given ticker and call dates."""
+    summarized_volatility = []
+    for call_date in call_dates:
+        before = df[(df['symbol'] == ticker) & 
+                    (df['date'] < call_date) & 
+                    (df['date'] >= call_date - pd.Timedelta(days=5))]
+        after = df[(df['symbol'] == ticker) & 
+                   (df['date'] > call_date) & 
+                   (df['date'] <= call_date + pd.Timedelta(days=5))]
+        avg_vol_before = before['volatility'].mean() * 100
+        avg_vol_after = after['volatility'].mean() * 100
+        summarized_volatility.append({
+            'date': call_date,
+            'Before Earnings Call': avg_vol_before,
+            'After Earnings Call': avg_vol_after
+        })
+    summarized_df = pd.DataFrame(summarized_volatility)
+    return summarized_df
+
+def plot_ticker_volatility(ax, summarized_df, ticker):
+    """Plot the average volatility before and after earnings calls for a given ticker."""
+    summarized_df.plot(kind='bar', ax=ax, rot=45, title=ticker)
+    ax.set_xlabel('Earnings Call Dates')
+    ax.set_ylabel('Avg Volatility (Std. Dev. of Daily Returns %)')
+    ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.0f}%'.format))
